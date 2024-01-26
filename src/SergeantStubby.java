34d3155ff89395325daea7a685860e66d5e6ee18
FinSmith198 <smithfinlay198@gmail.com>
2024-01-26 17:58:48 +0000

    fixed output of jar - added metaMF and Manifest file, as that was missing

--- src/SergeantStubby.java
+++ src/SergeantStubby.java
@@ -7,9 +7,6 @@ public class SergeantStubby {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("test");
-
-
         // create the bot instance with certain token
         Bot.getInstance((String) Config.getInstance("DDServerData.db").config.get("DISCORD_TOKEN"));
 
