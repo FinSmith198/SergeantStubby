6f606ef52c0ea5ab94eee42b57f717968f221d8c
FinSmith198 <smithfinlay198@gmail.com>
2025-05-15 11:17:01 +0100

    made build script (prototype)

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Directories
+SRC_DIR="src"
+#RESOURCES_DIR="resources"
+LIB_DIR="lib"
+BUILD_DIR="build"
+
+# create build directory
+mkdir -p "$BUILD_DIR"
+
+# classpath (include all jars in lib directory)
+CP=$(find "$LIB_DIR" -name "*.jar" | paste -sd ":" -)
+
+# compile Java files
+find "$SRC_DIR" -name "*.java" > sources.txt
+javac -d "$BUILD_DIR" -cp "$CP" @sources.txt
+rm -f sources.txt
+
+# copy resources to buildpath (the Icons)
+cp -r "$RESOURCES_DIR"/* "$BUILD_DIR"/
+
+# run the main class with java -cp (adjust package path accordingly)
+MAIN_CLASS="SergeantStubby"
+java -cp "$BUILD_DIR:$CP" "$MAIN_CLASS"
+
