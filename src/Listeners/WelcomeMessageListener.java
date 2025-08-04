e1abf5fc023de8015e26613431eac2415078abde
FinSmith198 <smithfinlay198@gmail.com>
2025-08-04 09:49:22 +0100

    REP role on server join no longer asks to join clan

--- src/Listeners/WelcomeMessageListener.java
+++ src/Listeners/WelcomeMessageListener.java
@@ -2,20 +2,14 @@ package Listeners;
 
 import Classes.Bot;
 import Classes.Config;
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.entities.channel.concrete.PrivateChannel;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
-import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleAddEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.jetbrains.annotations.NotNull;
 
-import java.awt.*;
 import java.sql.SQLException;
-import java.util.function.Consumer;
 
 public class WelcomeMessageListener extends ListenerAdapter {
 
@@ -29,12 +23,24 @@ public class WelcomeMessageListener extends ListenerAdapter {
 
         // get the member who joined, and the current set welcome channel
         Member member = event.getMember();
-        TextChannel welcomeChannel = event.getJDA().getTextChannelById(Config.WELCOME_CHANNEL);
 
+        if (member.getUser().isBot())
+            return;
+
+        TextChannel welcomeChannel = event.getJDA().getTextChannelById(Config.WELCOME_CHANNEL);
         assert welcomeChannel != null;
 
+        String message;
+        Role representativeRole = event.getJDA().getRoleById(1001465501563752593L);
+
+        // if user chooses rep, don't ask to join clan
+        if (member.getRoles().contains(representativeRole))
+            message = "Welcome to the Devil Dogs discord server <@"+member.getId()+">! If there is anything we can help you with, feel free to shoot any of our Staff Sergeants a message.";
+        else
+            message = "Welcome to the Devil Dogs discord server <@"+member.getId()+">! If you are interested in joining our clan, please find out recruitment channel at https://discord.com/channels/1001456689851146331/1001520952489807983 and it will walk you through our process. If there is anything we can help you with, feel free to shoot any of our Staff Sergeants a message.";
+
         // send the message
-        welcomeChannel.sendMessage("Welcome to the Devil Dogs discord server <@"+member.getId()+">! If you are interested in joining our clan, please find out recruitment channel at https://discord.com/channels/1001456689851146331/1001520952489807983 and it will walk you through our process. If there is anything we can help you with, feel free to shoot any of our Staff Sergeants a message.").queue();
+        welcomeChannel.sendMessage(message).queue();
 
         // try to add them to DB
         try {
